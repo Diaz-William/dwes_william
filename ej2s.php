61b56ab2ed5e72215ae6c0cc2ff4e411fa45dcda
William Diaz <williameliasdiazsantana@gmail.com>
2024-09-19 14:08:10 +0200

    cambio13

--- ej2s.php
+++ ej2s.php
@@ -9,16 +9,12 @@
             $octetos = explode(".", $ip);
 
             print("La ip " . $ip . " en binario es ");
-
             for ($i = 0; $i < count($octetos); $i++) {
                 $octetos[$i] = decbin($octetos[$i]);
             }
-
             for ($i = 0; $i < count($octetos); $i++) {
-                if (strlen($octetos[$i]) < 8) {
-                    for ($j = 0; $j <= (8 - strlen($octetos[$i])); $j++) {
-                        $octetos[$i] = "0" . $octetos[$i];
-                    }
+                while (strlen($octetos[$i]) < 8) {
+					$octetos[$i] = "0" . $octetos[$i];
                 }
                 print($octetos[$i]);
                 if ($i < count($octetos) - 1) {
