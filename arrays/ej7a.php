1d147d887a34151e8e10e3e40d88208440b50656
William Diaz <williameliasdiazsantana@gmail.com>
2024-09-23 11:45:01 +0200

    cambio18

---
+++ arrays/ej7a.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ej7_Arrays</title>
+        <style>
+            table, th, td {
+                border: 1px solid black;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+            $alumnos = array("nombre" => "William", "edad" => "21");
+        ?>
+    </body>
+</html>
