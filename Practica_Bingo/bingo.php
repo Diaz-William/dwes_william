233e1ddd2488de7ba1a30a4edb05a5885fe81941
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-02 10:07:56 +0200

    cambio40

--- Practica_Bingo/bingo.php
+++ Practica_Bingo/bingo.php
@@ -2,32 +2,19 @@
 <html>
     <body>
         <?php
-            $jugadores = array(
-                "Jugador1" => array(
-                    "Carton1" => array("numeros" => array(), "aciertos" => 0),
-                    "Carton2" => array("numeros" => array(), "aciertos" => 0),
-                    "Carton3" => array("numeros" => array(), "aciertos" => 0)
-                ),
-                "Jugador2" => array(
-                    "Carton1" => array("numeros" => array(), "aciertos" => 0),
-                    "Carton2" => array("numeros" => array(), "aciertos" => 0),
-                    "Carton3" => array("numeros" => array(), "aciertos" => 0)
-                ),
-                "Jugador3" => array(
-                    "Carton1" => array("numeros" => array(), "aciertos" => 0),
-                    "Carton2" => array("numeros" => array(), "aciertos" => 0),
-                    "Carton3" => array("numeros" => array(), "aciertos" => 0)
-                ),
-                "Jugador4" => array(
-                    "Carton1" => array("numeros" => array(), "aciertos" => 0),
-                    "Carton2" => array("numeros" => array(), "aciertos" => 0),
-                    "Carton3" => array("numeros" => array(), "aciertos" => 0)
-                )
-            );
+            $jugadores = array();
+            $cantJugadores = 1;
+            $cantCartones = 1;
             
+            for ($i = 1; $i <= $cantJugadores; $i++) {
+                $jugadores["Jugador" . $i] = array();
+                for ($j = 1; $j <= $cantCartones; $j++) {
+                    $jugadores["Jugador" . $i]["Carton" . $j] = array("numeros" => array(), "aciertos" => 0);
+                }
+            }
+            //BOLAS BOMBO
             $bolas = range(1, 60);
             shuffle($bolas);
-
             function rellenarCartones(&$jugadores) {
                 foreach ($jugadores as $jugador => &$cartones) {
                     foreach ($cartones as $carton => &$datosCarton) {
@@ -37,10 +24,8 @@
                     }
                 }
             }
-
             function contarAciertos(&$jugadores, $bolas) {
                 $salir = false;
-
                 foreach ($bolas as $bola) {
                     foreach ($jugadores as $jugador => &$cartones) {
                         foreach ($cartones as $carton => &$datosCarton) {
@@ -49,9 +34,8 @@
                                     $datosCarton['aciertos']++;
                                 }
                             }
-    
                             if ($datosCarton['aciertos'] == 15) {
-                                echo "$jugador ha ganado con el $carton!<br>";
+                                echo "$jugador ha ganado con el $carton<br>";
                                 $salir = true;
                                 break;
                             }
@@ -68,28 +52,46 @@
                     }
                 }
             }
-
             function mostrarBolas($bolas) {
+            	//Contenedor visual del bombo
+    			echo "<div name='contenedor' style='border: 1px solid black; border-radius: 25px; height:50vh; padding: 50px;'>";
                 for ($i = 0; $i < count($bolas); $i++) {
-                    echo $bolas[$i];
-
+                    echo "<img src='imagenes/$bolas[$i].PNG'>";
                     if ($i < count($bolas) - 1) {
                         echo ", ";
                     }
                 }
-            }            
-
+                echo "</div>";
+                echo "<br><br>";
+            }
             rellenarCartones($jugadores);
-
-            echo "Bolas sacadas: ";
             mostrarBolas($bolas);
-            echo ".<br><br>";
-
             contarAciertos($jugadores, $bolas);
-
-            echo "<pre>";
-            print_r($jugadores);
-            echo "</pre>";
+            visualizar($jugadores);
+            function visualizar($jugadores){
+              echo "<div style='margin-bottom: 30px;'>";
+              foreach ($jugadores as $jugador => $cartones) {
+                  echo "<h2 style='text-align: center;'>$jugador</h2>";
+                  echo "<div style='display: flex; flex-wrap: wrap; justify-content: space-between; gap: 10px; align-items: center;'>";
+                  foreach ($cartones as $carton => $datosCarton) {
+                      echo "<table border='1' style='border-collapse: collapse;'>";
+                      // Visualizar los cartones de cada jugador en una tabla 3x5
+                      $numeros = $datosCarton['numeros'];
+                      for ($fila = 0; $fila < 3; $fila++) {
+                          echo "<tr>";
+                          for ($columna = 0; $columna < 5; $columna++) {
+                              // Calcular el índice correcto del array
+                              $indice = $fila * 5 + $columna;
+                              echo "<td style='padding: 10px; text-align: center;'>{$numeros[$indice]}</td>";
+                          }
+                          echo "</tr>";
+                      }
+                      echo "</table>";
+                  }
+                  echo "</div>";
+              }
+              echo "</div>";
+          }
         ?>
     </body>
 </html>
