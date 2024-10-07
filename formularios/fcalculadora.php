b7bb3742668a7359192c441948f177ce1c223bd0
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-07 09:59:28 +0200

    .

--- formularios/fcalculadora.php
+++ formularios/fcalculadora.php
@@ -3,10 +3,28 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Calculadora.php</title>
+        <title>FCalculadora</title>
     </head>
     <body>
         <h1>Calculadora</h1>
+        <form action="calculadora.php" method="get">
+            <label>Número 1</label>
+            <input type="number" name="num1"><br><br>
+            <label>Número 2</label>
+            <input type="number" name="num2">
+            <br><br>
+            <label>Selecciona operación:</label><br><br>
+            <input type="radio" id="suma" name="operacion" value="suma">
+            <label for="suma">Suma</label><br><br>
+            <input type="radio" id="resta" name="operacion" value="resta">
+            <label for="resta">Resta</label><br><br>
+            <input type="radio" id="producto" name="operacion" value="producto">
+            <label for="producto">Producto</label><br><br>
+            <input type="radio" id="division" name="operacion" value="division">
+            <label for="division">División</label><br><br>
+            <input type="submit" value="Enviar">
+            <input type="reset" value="Borrar">
+        </form>
         <?php
             $resultado = 0;
 
