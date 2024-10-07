6712bca275a602a5390bd92a3222eb0d8d58b202
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-07 09:41:15 +0200

    .

--- formularios/calculadora.php
+++ formularios/calculadora.php
@@ -1,3 +1,33 @@
-<?php
-    print_r($_REQUEST);
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Calculadora.php</title>
+    </head>
+    <body>
+        <?php
+            $resultado = 0;
+
+            if ($_REQUEST["operacion"] == "suma") {
+                $resultado = $_REQUEST["num1"] + $_REQUEST["num2"];
+                echo "<p>Resultado de la operación: " . $_REQUEST["num1"] . " + " . $_REQUEST["num2"] . " = " . $resultado ."</p>";;
+            }
+
+            if ($_REQUEST["operacion"] == "resta") {
+                $resultado = $_REQUEST["num1"] - $_REQUEST["num2"];
+                echo "<p>Resultado de la operación: " . $_REQUEST["num1"] . " - " . $_REQUEST["num2"] . " = " . $resultado ."</p>";;
+            }
+
+            if ($_REQUEST["operacion"] == "producto") {
+                $resultado = $_REQUEST["num1"] * $_REQUEST["num2"];
+                echo "<p>Resultado de la operación: " . $_REQUEST["num1"] . " * " . $_REQUEST["num2"] . " = " . $resultado ."</p>";;
+            }
+
+            if ($_REQUEST["operacion"] == "division") {
+                $resultado = $_REQUEST["num1"] / $_REQUEST["num2"];
+                echo "<p>Resultado de la operación: " . $_REQUEST["num1"] . " / " . $_REQUEST["num2"] . " = " . $resultado ."</p>";;
+            }
+        ?>
+    </body>
+</html>
