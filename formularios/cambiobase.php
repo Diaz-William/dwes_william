fdf8ec6562d336eb36e37dba684300bcb9a5676c
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-08 10:38:11 +0200

    .

--- formularios/cambiobase.php
+++ formularios/cambiobase.php
@@ -6,6 +6,70 @@
         <title>Document</title>
     </head>
     <body>
-        
+        <h1>Conversor Númerico</h1>
+
+        <?php
+            $convertido = $num = 0;
+            $base = "";
+
+            $base = test_input($_REQUEST["base"]);
+            $num = intval(test_input($_REQUEST["num"]));
+
+            echo "<label>Número Decimal</label>";
+            echo "<input type='text' name='num' value='$num' readonly><br><br>";
+
+            switch ($base) {
+                case "binario":
+                    $convertido = convertir($num, 2);
+                    escribir($base, $convertido);
+                    break;
+                case "octal":
+                    $convertido = convertir($num, 8);
+                    escribir($base, $convertido);
+                    break;
+                case "hexadecimal":
+                    $convertido = convertir($num, 16);
+                    escribir($base, $convertido);
+                    break;
+                case "todas":
+                    tablaTodas($num);
+                    break;
+            }
+
+            function convertir($num, $base) {
+                return base_convert($num, 10, $base);
+            }
+
+            function escribir($base, $convertido) {
+                echo "<label>$base</label>";
+                echo "<input type='text' name='num' value='$convertido' readonly><br><br>";
+            }
+
+            function tablaTodas($num) {
+                $convertido = 0;
+
+                echo "<table>";
+                echo "<tr>";
+                $convertido = convertir($num, 2);
+                echo "<th>Binario</th><td>$convertido</td>";
+                echo "</tr>";
+                echo "<tr>";
+                $convertido = convertir($num, 8);
+                echo "<th>Octal</th><td>$convertido</td>";
+                echo "</tr>";
+                echo "<tr>";
+                $convertido = convertir($num, 16);
+                echo "<th>Hexadecimal</th><td>$convertido</td>";
+                echo "</tr>";
+                echo "</table>";
+            }
+
+            function test_input($data) {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
+            }
+        ?>
     </body>
 </html>
