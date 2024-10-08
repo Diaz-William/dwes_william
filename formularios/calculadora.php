adf0cee83cb2ffd6d0d8ff66979ffb26bb8efafe
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-08 09:11:18 +0200

    .

--- formularios/calculadora.php
+++ formularios/calculadora.php
@@ -9,25 +9,47 @@
         <h1>Calculadora</h1>
         <?php
             $resultado = 0;
+            $operacion = $_REQUEST["operacion"];
+            $num1 = $_REQUEST["num1"];
+            $num2 = $_REQUEST["num2"];
 
-            if ($_REQUEST["operacion"] == "suma") {
-                $resultado = $_REQUEST["num1"] + $_REQUEST["num2"];
-                echo "<p>Resultado de la operación: " . $_REQUEST["num1"] . " + " . $_REQUEST["num2"] . " = " . $resultado ."</p>";;
+            switch ($operacion) {
+                case "suma":
+                    $resultado = suma($num1, $num2);
+                    echo "<p>Resultado de la operación: $num1 + $num2 = $resultado</p>";
+                    break;
+                case "resta":
+                    $resultado = resta($num1, $num2);
+                    echo "<p>Resultado de la operación: $num1 - $num2 = $resultado</p>";
+                    break;
+                case "producto":
+                    $resultado = producto($num1, $num2);
+                    echo "<p>Resultado de la operación: $num1 * $num2 = $resultado</p>";
+                    break;
+                case "division":
+                    $resultado = division($num1, $num2);
+                    echo "<p>Resultado de la operación: $num1 / $num2 = $resultado</p>";
+                    break;
             }
 
-            if ($_REQUEST["operacion"] == "resta") {
-                $resultado = $_REQUEST["num1"] - $_REQUEST["num2"];
-                echo "<p>Resultado de la operación: " . $_REQUEST["num1"] . " - " . $_REQUEST["num2"] . " = " . $resultado ."</p>";;
+            function suma($num1, $num2) {
+                return $num1 + $num2;
             }
 
-            if ($_REQUEST["operacion"] == "producto") {
-                $resultado = $_REQUEST["num1"] * $_REQUEST["num2"];
-                echo "<p>Resultado de la operación: " . $_REQUEST["num1"] . " * " . $_REQUEST["num2"] . " = " . $resultado ."</p>";;
+            function resta($num1, $num2) {
+                return $num1 - $num2;
             }
 
-            if ($_REQUEST["operacion"] == "division") {
-                $resultado = $_REQUEST["num1"] / $_REQUEST["num2"];
-                echo "<p>Resultado de la operación: " . $_REQUEST["num1"] . " / " . $_REQUEST["num2"] . " = " . $resultado ."</p>";;
+            function producto($num1, $num2) {
+                return $num1 * $num2;
+            }
+
+            function division($num1, $num2) {
+                if ($num2 == 0) {
+                    return "Error: división por cero";
+                }else {
+                    return $num1 / $num2;
+                }
             }
         ?>
     </body>
