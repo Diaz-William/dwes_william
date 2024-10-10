fcbf89960197c05018afc546f2b54804ffee4ed6
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-10 17:18:35 +0200

    .

--- formularios/datos.php
+++ formularios/datos.php
@@ -13,23 +13,34 @@
     <body>
         <h1>Datos Alumnos</h1>
         <?php
-            $nombre = $apellido1 = $apellido2 = $emial = $sexo = "";
+            $nombre = $apellido1 = $apellido2 = $email = $sexo = "";
 
             $nombre = test_input($_REQUEST["nombre"]);
             $apellido1 = test_input($_REQUEST["apellido1"]);
             $apellido2 = test_input($_REQUEST["apellido2"]);
-            $emial = test_input($_REQUEST["email"]);
+            $email = test_input($_REQUEST["email"]);
             $sexo = test_input($_REQUEST["sexo"]);
 
-            echo "<table>";
-            echo "<tr><th>Nombre</th><th>Apellidos</th><th>Email</th><th>Sexo</th></tr>";
-            echo "<tr>";
-            echo "<td>$nombre</td>";
-            echo "<td>$apellido1 $apellido2</td>";
-            echo "<td>$email</td>";
-            echo "<td>$sexo</td>";
-            echo "</tr>";
-            echo "</table>";
+            imprimir($nombre, $apellido1, $apellido2, $email, $sexo);
+            guardarArchivo($nombre, $apellido1, $apellido2, $email, $sexo);
+
+            function imprimir($nombre, $apellido1, $apellido2, $email, $sexo) {
+                echo "<table>";
+                echo "<tr><th>Nombre</th><th>Apellidos</th><th>Email</th><th>Sexo</th></tr>";
+                echo "<tr>";
+                echo "<td>$nombre</td>";
+                echo "<td>$apellido1 $apellido2</td>";
+                echo "<td>$email</td>";
+                echo "<td>$sexo</td>";
+                echo "</tr>";
+                echo "</table>";
+            }
+
+            function guardarArchivo($nombre, $apellido1, $apellido2, $email, $sexo) {
+                $file = fopen("datos.txt", "a");
+                fwrite($file, "$nombre, $apellido1, $apellido2, $email, $sexo" . PHP_EOL);
+                fclose($file);
+            }
 
             function test_input($data) {
                 $data = trim($data);
