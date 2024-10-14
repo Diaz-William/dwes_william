c09cd331f1a4877450fa6a0dfb7dae9bda62e82a
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-14 10:35:56 +0200

    .

---
+++ files/fichero3.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>fichero3.php</title>
+    </head>
+    <body>
+    <h1>Datos Alumnos</h1>
+    <?php
+        function imprimir() {
+            $fichero = fopen("alumnos1.txt", "r") or die("No se ha podido abrir el archivo");
+            $datos = array();
+            $datos = explode(" ", $fichero);
+            
+            /*while(!feof($fichero)) {
+                echo fgets($fichero);
+            }*/
+
+            echo "<pre>";
+            echo print_r( $datos );
+            echo "</pre>";
+            
+            fclose($fichero);
+        }
+
+        function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+    ?>
+    </body>
+</html>
