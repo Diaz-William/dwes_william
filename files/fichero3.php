3eeffbfce2a7158e42bebc6b27f0704f1669dd7b
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-14 10:55:05 +0200

    .

--- files/fichero3.php
+++ files/fichero3.php
@@ -12,20 +12,25 @@
 
         function imprimir() {
             $fichero = fopen("alumnos1.txt", "r") or die("No se ha podido abrir el archivo");
+            $datos = file("alumnos1.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
             
-            $nombre = trim(file_get_contents("alumnos1.txt", FALSE, NULL, 1, 40));
+            /*$nombre = trim(file_get_contents("alumnos1.txt", FALSE, NULL, 1, 40));
             $apellido1 = trim(file_get_contents("alumnos1.txt", FALSE, NULL, 41, 81));
             $apellido2 = trim(file_get_contents("alumnos1.txt", FALSE, NULL, 82, 123));
             $nacimiento = trim(file_get_contents("alumnos1.txt", FALSE, NULL, 124, 133));
-            $localidad = trim(file_get_contents("alumnos1.txt", FALSE, NULL, 134, 160));
+            $localidad = trim(file_get_contents("alumnos1.txt", FALSE, NULL, 134, 160));*/
             
             fclose($fichero);
 
-            echo "$nombre <br>";
+            echo "<pre>";
+            echo print_r($datos);
+            echo "</pre>";
+
+            /*echo "$nombre <br>";
             echo "$apellido1 <br>";
             echo "$apellido2 <br>";
             echo "$nacimiento <br>";
-            echo "$localidad <br>";
+            echo "$localidad <br>";*/
         }
 
         function test_input($data) {
