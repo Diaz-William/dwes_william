9767bcc7485fde1bb20919a08e66008ed0c9a28b
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-14 10:08:13 +0200

    .

--- files/fichero1.php
+++ files/fichero1.php
@@ -26,34 +26,35 @@
         <input type="submit" value="Enviar">
         <input type="reset" value="Borrar">
     </form>
-        <?php
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $nombre = test_input($_POST["nombre"]);
-                $apellido1 = test_input($_POST["apellido1"]);
-                $apellido2 = test_input($_POST["apellido2"]);
-                $nacimiento = test_input($_POST["nacimiento"]);
-                $localidad = test_input($_POST["localidad"]);
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $nombre = test_input($_POST["nombre"]);
+            $apellido1 = test_input($_POST["apellido1"]);
+            $apellido2 = test_input($_POST["apellido2"]);
+            $nacimiento = test_input($_POST["nacimiento"]);
+            $localidad = test_input($_POST["localidad"]);
 
-                $nombre = str_pad($nombre, 40, " ", STR_PAD_RIGHT);
-                $apellido1 = str_pad($apellido1,40," ", STR_PAD_RIGHT);
-                $apellido2 = str_pad($apellido2, 41, " ", STR_PAD_RIGHT);
-                $nacimiento = str_pad($nacimiento,9," ", STR_PAD_RIGHT);
-                $localidad = str_pad($localidad,24," ", STR_PAD_RIGHT);
+            $nombre = str_pad($nombre, 40, " ", STR_PAD_RIGHT);       // Posición 1 a 40
+            $apellido1 = str_pad($apellido1, 41, " ", STR_PAD_RIGHT); // Posición 41 a 81
+            $apellido2 = str_pad($apellido2, 42, " ", STR_PAD_RIGHT); // Posición 82 a 123
+            $nacimiento = str_pad($nacimiento, 10, " ", STR_PAD_RIGHT); // Posición 124 a 133
+            $localidad = str_pad($localidad, 27, " ", STR_PAD_RIGHT); // Posición 134 a 160
 
-                guardarArchivo($nombre, $apellido1, $apellido2, $nacimiento, $localidad);
-            }
-            function guardarArchivo($nombre, $apellido1, $apellido2, $nacimiento, $localidad) {
-                $file = fopen("alumnos1.txt", "a");
-                fwrite($file, "$nombre$apellido1$apellido2$nacimiento$localidad" . PHP_EOL);
-                fclose($file);
-            }
+            guardarArchivo($nombre, $apellido1, $apellido2, $nacimiento, $localidad);
+        }
 
-            function test_input($data) {
-                $data = trim($data);
-                $data = stripslashes($data);
-                $data = htmlspecialchars($data);
-                return $data;
-            }
-        ?>
+        function guardarArchivo($nombre, $apellido1, $apellido2, $nacimiento, $localidad) {
+            $file = fopen("alumnos1.txt", "a");
+            fwrite($file, "$nombre$apellido1$apellido2$nacimiento$localidad" . PHP_EOL);
+            fclose($file);
+        }
+
+        function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+    ?>
     </body>
 </html>
