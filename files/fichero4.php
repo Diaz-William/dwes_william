7fe6e933ed0ad172ca5eee5ffb8326de1a9af5fa
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-15 09:08:10 +0200

    .

--- files/fichero4.php
+++ files/fichero4.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>fichero3.php</title>
+        <title>fichero4.php</title>
         <style>
             table, th, td {
                 border:1px solid black;
@@ -16,10 +16,11 @@
         imprimir();
 
         function imprimir() {
-            $fichero = fopen("alumnos1.txt", "r") or die("No se ha podido abrir el archivo");
-            $datos = file("alumnos1.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+            $fichero = fopen("alumnos2.txt", "r") or die("No se ha podido abrir el archivo");
+            $datos = file("alumnos2.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
             fclose($fichero);
             $lineas = count($datos);
+            $datosLinea = array();
 
             echo "<table>";
             echo "<tr>";
@@ -31,17 +32,11 @@
             echo "</tr>";
 
             foreach ($datos as $x) {
-                $nombre = trim(substr($x, 0, (40 - 0)));
-                $apellido1 = trim(substr($x, 40, (81 - 40)));
-                $apellido2 = trim(substr($x, 81, (123 - 81)));
-                $nacimiento = trim(substr($x, 123, (133 - 123)));
-                $localidad = trim(substr($x, 133, (160 - 133)));
                 echo "<tr>";
-                echo "<td>$nombre</td>";
-                echo "<td>$apellido1</td>";
-                echo "<td>$apellido2</td>";
-                echo "<td>$nacimiento</td>";
-                echo "<td>$localidad</td>";
+                $datosLinea = explode("##", $x);
+                foreach ($datosLinea as $y) {
+                    echo "<td>$y</td>";
+                }
                 echo "</tr>";
             }
 
