896586462b69bcee060ccb5b43810f063be30dbe
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-15 08:55:54 +0200

    .

--- files/fichero3.php
+++ files/fichero3.php
@@ -4,6 +4,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>fichero3.php</title>
+        <style>
+            table, th, td {
+                border:1px solid black;
+            }
+        </style>
     </head>
     <body>
     <h1>Datos Alumnos</h1>
@@ -13,24 +18,31 @@
         function imprimir() {
             $fichero = fopen("alumnos1.txt", "r") or die("No se ha podido abrir el archivo");
             $datos = file("alumnos1.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-            
-            $nombre = trim(substr($datos[0], 0, (40 - 0)));
-            $apellido1 = trim(substr($datos[0], 40, (81 - 40)));
-            $apellido2 = trim(substr($datos[0], 81, (123 - 81)));
-            $nacimiento = trim(substr($datos[0], 123, (133 - 123)));
-            $localidad = trim(substr($datos[0], 133, (160 - 133)));
-            
             fclose($fichero);
 
-            echo "<pre>";
-            echo print_r($datos);
-            echo "</pre>";
-
-            echo "$nombre <br>";
-            echo "$apellido1 <br>";
-            echo "$apellido2 <br>";
-            echo "$nacimiento <br>";
-            echo "$localidad <br>";
+            echo "<table>";
+            echo "<tr>";
+            echo "<th>Nombre</th>";
+            echo "<th>Apellido 1</th>";
+            echo "<th>Apellido 2</th>";
+            echo "<th>Nacimiento</th>";
+            echo "<th>Localidad</th>";
+            echo "</tr>";
+            
+            foreach ($datos as $x) {
+                $nombre = trim(substr($x, 0, (40 - 0)));
+                $apellido1 = trim(substr($x, 40, (81 - 40)));
+                $apellido2 = trim(substr($x, 81, (123 - 81)));
+                $nacimiento = trim(substr($x, 123, (133 - 123)));
+                $localidad = trim(substr($x, 133, (160 - 133)));
+                echo "<tr>";
+                echo "<td>$nombre</td>";
+                echo "<td>$apellido1</td>";
+                echo "<td>$apellido2</td>";
+                echo "<td>$nacimiento</td>";
+                echo "<td>$localidad</td>";
+                echo "</tr>";
+            }
         }
 
         function test_input($data) {
