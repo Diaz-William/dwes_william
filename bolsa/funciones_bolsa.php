a8cabfcf3c660f4a27c8518fa770a9be9bb78db6
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-16 10:35:21 +0200

    .

--- bolsa/funciones_bolsa.php
+++ bolsa/funciones_bolsa.php
@@ -6,8 +6,12 @@
         return $datos;
     }
     function mostrarDatos($datos) {
+        $linea = array();
         foreach ($datos as $x) {
-            echo "$x<br>";
+            $linea = linea($x);
+            foreach ($linea as $y) {
+                echo str_pad($y, 10, " ", STR_PAD_RIGHT);
+            }
         }
     }
 
@@ -32,8 +36,25 @@
             echo $datos . "<br><br>";
             echo $datosValor;
         }else {
+            echo "<br><br>";
             echo "No se ha encontrado un valor con el nombre $valor";
         }
+    }
+
+    function linea($linea) {
+        $data = array();
+        if($linea != "") {
+            $data[0] = test_input(substr($linea,0,23));
+            $data[1] = test_input(substr($linea,23,8));
+            $data[2] = test_input(substr($linea,32,8));
+            $data[3] = test_input(substr($linea,40,8));
+            $data[4] = test_input(substr($linea,48,11));
+            $data[5] = test_input(substr($linea,60,8));
+            $data[6] = test_input(substr($linea,69,8));
+            $data[7] = test_input(substr($linea,78,12));
+            $data[8] = test_input(substr($linea,91,8));
+        }
+        return $data;
     }
 
     function test_input($data) {
