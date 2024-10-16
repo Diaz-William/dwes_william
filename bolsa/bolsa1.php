85990d82157a7ded4d3cdd551964b3cfa8816aa4
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-16 09:34:08 +0200

    .

--- bolsa/bolsa1.php
+++ bolsa/bolsa1.php
@@ -10,9 +10,7 @@
     <?php
         include 'funciones_bolsa.php';
 
-        $fichero = fopen("ibex35.txt", "r") or die("No se ha podido abrir el archivo");
-        $datos = file("ibex35.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-        fclose($fichero);
+        $datos = obtenerDatos();
 
         mostrarDatos($datos);
     ?>
--- bolsa/funciones_bolsa.php
+++ bolsa/funciones_bolsa.php
@@ -1,4 +1,10 @@
 <?php
+    function obtenerDatos() {
+        $fichero = fopen("ibex35.txt", "r") or die("No se ha podido abrir el archivo");
+        $datos = file("ibex35.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        fclose($fichero);
+        return $datos;
+    }
     function mostrarDatos($datos) {
         foreach ($datos as $x) {
             echo "$x<br>";
