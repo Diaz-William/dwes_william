8e518008901e6440ebf16e1a08b1ccc78714ea86
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-21 10:54:15 +0200

    .

---
+++ reto_ficheros/ficheros.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+            $xml = simplexml_load_file('pronosticotiempoLasRozas.xml') or die("Error: No se puede crear el objeto");
+
+            echo $xml -> provincia;
+        ?>
+    </body>
+</html>
