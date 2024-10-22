a5299f49e5a5360275449151dade4592969cca3c
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-22 10:57:07 +0200

    .

--- reto_ficheros/funciones.php
+++ reto_ficheros/funciones.php
@@ -1,8 +1,7 @@
 <?php
-    //include 'errores_sistema.php';
-    //set_error_handler("error_function");
     function obtenerXML($nombre) {
         $nombre += ".xml";
+        var_dump($nombre);
         $xml = simplexml_load_file($nombre) or die("Error: No se puede crear el objeto");
         return $xml;
     }
