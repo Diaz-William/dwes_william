c9d21affd4f62dc459ff416d4561600aaeaa74fb
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-23 13:30:01 +0200

    .

--- reto_ficheros/funciones.php
+++ reto_ficheros/funciones.php
@@ -6,41 +6,13 @@
         return $xml;
     }
 
-    function imprimirTabla($nombre) {
+    function imprimirTabla($nombre, $fechas) {
         echo "<table>";
         echo "<tr>";
         echo "<td>$nombre</td>";
-        echo "<td colspan='3'>fecha</td>";
-        echo "<td colspan='3'>fecha</td>";
-        echo "</tr>";
-        echo "<tr>";
-        echo "<td>Periodo</td>";
-        echo "<td>06-12h</td>";
-        echo "<td>06-12h</td>";
-        echo "<td>06-12h</td>";
-        echo "</tr>";
-        echo "<tr>";
-        echo "<td>Precipitacion</td>";
-        echo "<td>100%</td>";
-        echo "<td>100%</td>";
-        echo "<td>100%</td>";
-        echo "</tr>";
-        echo "<tr>";
-        echo "<td>Viento</td>";
-        echo "<td>SO 20</td>";
-        echo "<td>SO 20</td>";
-        echo "<td>SO 20</td>";
-        echo "</tr>";
-        echo "<tr>";
-        echo "<td>Sensacion</td>";
-        echo "<td>20</td>";
-        echo "<td>20</td>";
-        echo "<td>20</td>";
-        echo "</tr>";
-        echo "<tr>";
-        echo "<td>Temp</td>";
-        echo "<td>13/22</td>";
-        echo "<td>13/22</td>";
+        foreach ($fechas as $fecha) {
+            echo "<td colspan='3'>$fecha</td>";
+        }
         echo "</tr>";
         echo "</table>";
     }
