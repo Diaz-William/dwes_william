bf3d46fc235e2dda3b1f4d81c25daf5633f73461
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-23 13:22:43 +0200

    .

--- reto_ficheros/ficheros.php
+++ reto_ficheros/ficheros.php
@@ -16,14 +16,14 @@
             include 'errores_sistema.php';
             set_error_handler("error_function");
 
-            $xml = obtenerXML("pronosticotiempoLasRozas");
+            $xml1 = obtenerXML("pronosticotiempoLasRozas");
 
-            $nombre = $xml -> nombre;
+            $nombre = $xml1 -> nombre;
             $fechas = array();
             $probPrecipitacion = array();
 
 
-            foreach ($xml -> prediccion -> dia as $x) {
+            foreach ($xml1 -> prediccion -> dia as $x) {
                 array_push($fechas, $x['fecha']);
             }
 
