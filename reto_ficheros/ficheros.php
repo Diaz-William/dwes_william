391167765ad16d69fe70cf6a224e227808df8176
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-23 18:59:18 +0200

    .

--- reto_ficheros/ficheros.php
+++ reto_ficheros/ficheros.php
@@ -48,5 +48,12 @@
             echo "</tr>";
             echo "</table>";
         ?>
+        <?php
+            function obtenerXML($nombre) {
+                $nombre .= ".xml";
+                $xml = simplexml_load_file($nombre) or die("Error: No se puede crear el objeto");
+                return $xml;
+            }
+        ?>
     </body>
 </html>
