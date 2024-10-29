e2ac392967904ae904943967c7df8cecf4f74c63
William Diaz <williameliasdiazsantana@gmail.com>
2024-10-29 09:09:33 +0100

    .

--- dados_escalable/registrarse.php
+++ dados_escalable/registrarse.php
@@ -16,14 +16,11 @@
                         <B>JUEGO DADOS</B>
                     </div>
                     <div class="card-body">
-                        <b>Nombre: </b><input type='text' name='jug1' value='' size=25><br><br> 
-                        <b>Primer Apelliado: </b><input type='text' name='jug2' value='' size=25><br><br> 
-                        <b>Jugador 3: </b><input type='text' name='jug3' value='' size=25><br><br> 
-                        <b>Jugador 4: </b><input type='text' name='jug4' value='' size=25><br><br><br> 
-                        <b>Numero Dados: </b><input type='text' name='numdados' value='' size=5><br><br>
-                        <b>Pulsa para Tirar Dados:</b>
+                        <b>Nombre:</b><input type='text' name='nombre' value='' size=25><br><br> 
+                        <b>Primer Apelliado:</b><input type='text' name='apellido' value='' size=25><br><br> 
+                        <b>Pulsa registrarte:</b>
                         <div>
-                            <input type="submit" value="Tirar Dados" name="tirar" class="btn btn-warning disabled">
+                            <input type="submit" value="Registrarse" name="registrar" class="btn btn-warning disabled">
                         </div>
                     </div>
                 </div>
