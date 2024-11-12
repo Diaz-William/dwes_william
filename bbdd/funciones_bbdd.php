1dfa9cd5650276075452f9e644a3e8baa6dd4bb4
William Diaz <williameliasdiazsantana@gmail.com>
2024-11-12 10:24:21 +0100

    .

--- bbdd/funciones_bbdd.php
+++ bbdd/funciones_bbdd.php
@@ -125,6 +125,8 @@
             echo "<p>Se ha inserado al empledo $nombre con el dni $dni en el departamento $dpto</p>";
         } catch (PDOException $e) {
             deshacer($conn);
+
+            echo $e->getMessage();
             
             if ($e->getCode() == '23000' && strpos($e->getMessage(), '1062 Duplicate entry') !== false) {
                 error_function($e->getCode(), $e->getMessage(), $e->getFile(), $e->getLine(), "Error: El DNI '$dni' ya existe en la base de datos. Por favor, use un DNI diferente.");
