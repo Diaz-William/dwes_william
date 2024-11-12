3fc1aaeaca3d288c8addb2cd6920a16cd93256b6
William Diaz <williameliasdiazsantana@gmail.com>
2024-11-12 10:13:31 +0100

    .

--- bbdd/errores_sistema.php
+++ bbdd/errores_sistema.php
@@ -1,13 +1,13 @@
 <?php
     function error_function($error_level, $error_message, $error_file, $error_line, $custom_error_message = null){
         echo "<hr>";
-        if (!is_null($custom_error_message)) {
-            echo "<p>$custom_error_message</p>";
-        }else {
-            echo "Código de error: $error_level <br>";
-            echo "Mensaje de error: $error_message <br>";
-            echo "Archivo: $error_file <br>";
-            echo "Línea: $error_line <br>";
+        if ($custom_error_message) {
+            echo "<p>" . htmlspecialchars($custom_error_message) . "</p>";
+        } else {
+            echo "Código de error: " . htmlspecialchars($error_level) . "<br>";
+            echo "Mensaje de error: " . htmlspecialchars($error_message) . "<br>";
+            echo "Archivo: " . htmlspecialchars($error_file) . "<br>";
+            echo "Línea: " . htmlspecialchars($error_line) . "<br>";
         }
         echo "<hr>";
     }
