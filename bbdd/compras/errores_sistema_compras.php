2bd2a897ca30e3b047904d3465b723aeda5ead6b
William Diaz <williameliasdiazsantana@gmail.com>
2024-11-13 19:17:37 +0100

    .

--- bbdd/compras/errores_sistema_compras.php
+++ bbdd/compras/errores_sistema_compras.php
@@ -1,15 +1,15 @@
 <?php
-    function error_function($error_level, $error_message, $error_file, $error_line, $name_table = null){
+    function error_function($error_level, $error_message, $error_file, $error_line, $table = null){
         echo "<hr>";
-        if (!empty($name_table)) {
+        if (!empty($table)) {
             var_dump("dentro por tabla");
-            var_dump($name_table);
-            var_dump(!empty($name_table));
+            var_dump($table);
+            var_dump(!empty($table));
             if ($error_level == '23000' && strpos($error_message, '1062 Duplicate entry') !== false) {
                 var_dump("dentro por clave");
-                switch ($name_table) {
+                switch ($table) {
                     case 'emple':
-                        echo "Error: Ya existe un empleado con el dni introducido en la tabla $name_table.";
+                        echo "Error: Ya existe un empleado con el dni introducido en la tabla $table.";
                         break;
                     default:
                         # code...
