5eaf909f8d285de657a779b8f13216a191c02b4b
unknown <williameliasdiazsantana@gmail.com>
2024-11-20 09:03:34 +0100

    .

--- Eval1/media7.php
+++ Eval1/media7.php
@@ -15,7 +15,40 @@
       set_error_handler("error_function");
 
       // Comprobar si se han enviado los datos del formulario por el método POST.
-      if ($_SERVER["REQUEST_METHOD"] == "POST") {}
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Obtener los datos del formulario y declaración de variables.
+        $nombre1 = test_input($_POST["nombre1"]);
+        $nombre2 = test_input($_POST["nombre2"]);
+        $nombre3 = test_input($_POST["nombre3"]);
+        $nombre4 = test_input($_POST["nombre4"]);
+        $numcartas = intval(test_input($_POST["numcartas"]));
+        $nombres = array();
+        $jugadores = array();
+        $ganadores = array();
+
+        if (empty($nombre1) || empty($nombre2)) {
+          // Mostrar un error si no se ha introducido los dos primeros nombres de los jugadores.
+          trigger_error("Debe introducir los dos primeros nombres de los jugadores");
+        } else if ($cantDados < 2 || $cantDados > 10) {
+          // Mostrar un error si la cantidad de cartas no está en el rango permitido (2 - 10).
+          trigger_error("Debe introducir un número de cartas del 2 al 10");
+        } else {
+          // Guardar todos nombres introducidos en el array $nombres.
+          array_push($nombres, $nombre1, $nombre2, $nombre3, $nombre4);
+    
+          // Rellenar el array $jugadores con los nombres y sus datos.
+          $jugadores = rellenarJugadores($nombres);
+          // Repartir las cartas de los jugadores y realizar la suma.
+          $jugadores = repartirCartas($jugadores, $numcartas);
+    
+          // Obtener los ganadores.
+          $ganadores = obtenerGanadores($jugadores);
+          // Mostrar los resultados.
+          mostrarResultados($jugadores);
+          // Mostrar los ganadores y el total de ganadores.
+          mostrarGanadores($ganadores);
+        }
+      }
     ?>
   </body>
 </html>
---
+++ Eval1/media7func.php
@@ -0,0 +1,88 @@
+<?php
+//--------------------------------------------------------------------------
+    // Función para limpiar la entrada de datos del usuario.
+	function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+//--------------------------------------------------------------------------
+    // Función para rellenar los jugadores con sus nombres y sus datos.
+	function rellenarJugadores($nombres) {
+        $jugadores = array();
+
+        for ($i = 0; $i < count($nombres); $i++) { 
+            if (!empty($nombres[$i])) {
+                $jugadores[$nombres[$i]] = array("resultados" => array(), "suma" => 0);
+            }
+        }
+
+        return $jugadores;
+    }
+//--------------------------------------------------------------------------
+    // Función para tirar los dados de cada jugador.
+	function repartirCartas($jugadores, $numcartas) {
+        
+        foreach ($jugadores as $jugador => &$datos) {
+            for ($i = 0; $i < $numcartas; $i++) {
+                array_push($datos["resultados"], rand(1,6));
+            }
+            $datos["suma"] = array_sum($datos["resultados"]);
+        }
+
+        return $jugadores;
+    }
+//--------------------------------------------------------------------------
+	// Función para obtener todos los ganadores.
+	function obtenerGanadores($jugadores) {
+		$ganadores = array();
+        $mayor = 0;
+        
+		foreach ($jugadores as $jugador => $datos) {
+            if ($datos["suma"] > $mayor) {
+                $mayor = $datos["suma"];
+            }
+        }
+		
+		foreach ($jugadores as $jugador => $datos) {
+            if ($datos["suma"] == $mayor) {
+                $ganadores[$jugador] = $jugadores[$jugador];
+            }
+        }
+		
+		return $ganadores;
+	}
+//--------------------------------------------------------------------------
+    // Función para mostrar los resultados de los jugadores.
+	function mostrarResultados($jugadores) {
+		echo "<hr>";
+		echo "<table>";
+		
+        foreach ($jugadores as $jugador => $datos) {
+			echo "<tr>";
+			echo "<td>$jugador</td>";
+            foreach ($datos["resultados"] as $num) {
+                echo "<td><img src='images/$num.png' style='width: 50px; height: 50px; margin: 5px;'></td>";
+            }
+			echo "</tr>";
+        }
+		
+		echo "</table>";
+		echo "<hr>";
+		
+		foreach ($jugadores as $jugador => $datos) {
+            echo "<p>$jugador --> " . $datos["suma"] . "</p>";
+        }
+    }
+//--------------------------------------------------------------------------
+    // Función para mostrar los ganadores.
+	function mostrarGanadores($ganadores) {
+		echo "<hr>";
+		foreach ($ganadores as $jugador => $datos) {
+            echo "<p>Ganador --> $jugador</p>";
+        }
+				
+		echo "<p>Total de ganadores --> " . count($ganadores) . "</p>";
+	}
+//--------------------------------------------------------------------------
