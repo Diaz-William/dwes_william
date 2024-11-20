3734fab75ce0915717c00cd83b7ddd3b9979c4d1
unknown <williameliasdiazsantana@gmail.com>
2024-11-20 08:53:06 +0100

    .

--- Eval1/media7.php
+++ Eval1/media7.php
@@ -1,15 +1,21 @@
-<?php
-  /* Nombre Alumno:                          */
-	
-				
-				
-				
+<!DOCTYPE html>
+  <html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="author" content="William Diaz">
+    <title>media7.php</title>
+  </head>
+  <body>
+    <?php
+      // Incluir el archivo de funciones.
+      include 'media7func.php';
+      // Incluir el archivo de manejo de errores.
+      include 'errores.php';
+      // Establecer la función "error_function" para el manejo de errores.
+      set_error_handler("error_function");
 
-
-
-
-
-
-
-
-?>
+      // Comprobar si se han enviado los datos del formulario por el método POST.
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {}
+    ?>
+  </body>
+</html>
