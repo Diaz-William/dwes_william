159f3dfd744f4eadfe75485a7e2c0600d5f1bd94
William Diaz <williameliasdiazsantana@gmail.com>
2024-11-25 10:05:26 +0100

    .

--- cookies_sesiones/web2_sesion.php
+++ cookies_sesiones/web2_sesion.php
@@ -1,8 +1,9 @@
 <?php
     if (isset($_SESSION["usuario"])) {
         header("Location: ./inicio_sesion.php");
+    }else {
+        session_start();
     }
-    session_start();
 ?>
 
 <!DOCTYPE html>
