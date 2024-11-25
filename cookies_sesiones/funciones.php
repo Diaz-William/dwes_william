3320e6784c126dca194a5f375de251daccc36906
William Diaz <williameliasdiazsantana@gmail.com>
2024-11-25 08:51:03 +0100

    .

--- cookies_sesiones/funciones.php
+++ cookies_sesiones/funciones.php
@@ -71,7 +71,7 @@
     function cerrarSesion() {
         session_unset();
         session_destroy();
-        //setcookie("PHPSESSID", "", time() - 3600, "/");
+        setcookie("PHPSESSID", "", time() - 3600, "/");
         header("Location: ./inicio_sesion.php");
     }
 //--------------------------------------------------------------------------
