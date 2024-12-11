b500853c6c4f8c2756bc5d9c2bcf730b2dcd96c1
William Diaz <williameliasdiazsantana@gmail.com>
2024-12-11 08:55:26 +0100

    .

--- bbdd/pedidos/pe_altaped.php
+++ bbdd/pedidos/pe_altaped.php
@@ -20,6 +20,7 @@
         <title>Realizar pedidos</title>
     </head>
     <body>
+        <a href="./pe_inicio.php">Inicio</a>
         <h1>Usuario: <?php echo $_COOKIE["usuario"] ?></h1>
         <h2>Comprar Productos</h2>
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
@@ -27,6 +28,9 @@
             <input name="unidades" type="text">
             <br><br>
             <?php imprimirSeleccionProductosDisponibles(); ?>
+            <br><br>
+            <label for="numPago">Número de pago:</label>
+            <input type="text" name="numPago" id="numPago" placeholder="AA99999">
             <br><br>
             <input type="submit" name="enviar" id="enviar" value="Añadir">
             <input type="submit" name="comprar" id="comprar" value="Comprar">
@@ -39,14 +43,17 @@
             // Comprobar si se han enviado los datos del formulario por el método POST.
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 if (isset($_POST["enviar"])) {
-                    if (empty($_POST["producto"]) || empty($_POST["unidades"])) {
-                        trigger_error("Tiene que seleccionar las unidades del producto y el producto.");
-                    } else {
+                    if (empty($_POST["producto"]) || empty($_POST["unidades"]) || empty($_POST["numPago"])) {
+                        trigger_error("Tiene que seleccionar las unidades del producto, el producto y el número de pago.");
+                    }else if (preg_match("/^[a-z][a-z]\d{5}$/i", test_input($_POST["numPago"]))) {
                         $productCode = test_input($_POST["producto"]);
                         $unidades = intval(test_input($_POST["unidades"]));
+                        $checkNumber = test_input($_POST["numPago"]);
                         guardarProductoCookies($productCode, $unidades);
+                    }else {
+                        trigger_error("El número de pago tiene un formato incorrecto");
                     }
-                } else if (isset($_POST["comprar"])) {
+                }else if (isset($_POST["comprar"])) {
                     comprarProductoSesionCookies();
                 }else if (isset($_POST["cerrar"])) {
                     cerrarSesionCookies();
