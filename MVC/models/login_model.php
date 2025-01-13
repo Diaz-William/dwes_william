0a2d6b0a255dbd22e0aebfe230653c4c8a3f88ae
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-13 12:33:41 +0100

    .

--- MVC/models/login_model.php
+++ MVC/models/login_model.php
@@ -2,6 +2,7 @@
     function comprobar($email, $password) {
         try {
             $conexion = conectar();
+            var_dump($conexion);
             $stmt = $conexion->prepare("SELECT 1 FROM RCLIENTES WHERE EMAIL = :EMAIL AND IDCLIENTE = :IDCLIENTE");
             $stmt->bindParam(':EMAIL', $email);
             $stmt->bindParam(':IDCLIENTE', $password);
