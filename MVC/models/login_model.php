bfd57d3457fafb102a66b4a7d355c4f942a406b5
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-14 10:09:04 +0100

    .

--- MVC/models/login_model.php
+++ MVC/models/login_model.php
@@ -8,7 +8,7 @@
             $stmt->execute();
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
             $result = $stmt->fetchAll();
-            $result = $result !== false ? $result[0] . " " . $result[1] : false;
+            $result = $result !== false ? $result["NOMBRE"] . " " . $result["APELLIDO"] : false;
             
             if (comprobarPendientePago($email, $password, $conexion) && $result !== false) {
                 $result = "Pendiente de pago";
