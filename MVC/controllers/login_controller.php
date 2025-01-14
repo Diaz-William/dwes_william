24725df39368a0dfdc5d091c28f2cf1406557d36
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-14 12:32:28 +0100

    .

--- MVC/controllers/login_controller.php
+++ MVC/controllers/login_controller.php
@@ -1,6 +1,7 @@
 <?php
     require_once("helpers/error_helper.php");
     set_error_handler("error_function");
+    require_once("helpers/data_helper.php");
     require_once("views/login_view.php");
     require_once("models/login_model.php");
     require_once("helpers/cookie_helper.php");
