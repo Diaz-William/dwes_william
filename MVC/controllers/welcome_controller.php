0ca645e7f55db868efb3f4026e02fc66f8924be1
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-14 10:39:20 +0100

    .

--- MVC/controllers/welcome_controller.php
+++ MVC/controllers/welcome_controller.php
@@ -1,11 +1,11 @@
 <?php
     require_once("../views/welcome_view.php");
-    require_once("helpers/cookie_helper.php");
+    require_once("../helpers/cookie_helper.php");
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (isset($_POST["cerrar"])) {
             cerrarSesionCookie();
-            header("Location: controllers/index.php");
+            header("Location: /index.php");
         }
     }
 ?>
