038d8672ad741517ad5447cf4717efa627f79cbd
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-14 10:37:44 +0100

    .

--- MVC/index.php
+++ MVC/index.php
@@ -2,5 +2,5 @@
     require_once("helpers/helpers.php");
     set_error_handler("error_function");    
     require_once("db/db.php");
-    require_once("controllers/movilmad_controller.php");
+    require_once("controllers/login_controller.php");
 ?>
