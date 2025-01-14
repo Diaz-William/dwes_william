4eaf105b89b5920f31c520881971408de5da9a10
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-14 10:38:45 +0100

    .

--- MVC/controllers/welcome_controller.php
+++ MVC/controllers/welcome_controller.php
@@ -1,5 +1,5 @@
 <?php
-    require_once("views/welcome_view.php");
+    require_once("../views/welcome_view.php");
     require_once("helpers/cookie_helper.php");
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
