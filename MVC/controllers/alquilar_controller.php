d92e0797a814dd1fa71a04a1a5adfa9c897d7fff
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-14 16:01:12 +0100

    .

--- MVC/controllers/alquilar_controller.php
+++ MVC/controllers/alquilar_controller.php
@@ -4,11 +4,6 @@
     set_error_handler("error_function");
     require_once("../models/alquilar_model.php");
     require_once("../helpers/cookie_helper.php");
-    require_once("../views/cesta_view.php");
-
-    $vehiculos = obtenerVehiculosDisponibles();
-
-    require_once("../views/alquilar_view.php");
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (isset($_POST["agregar"]) && !empty($_POST["vehiculos"])) {
@@ -17,4 +12,9 @@
             echo "Debe seleccionar un vehículo";
         }
     }
+
+    $vehiculos = obtenerVehiculosDisponibles();
+
+    require_once("../views/cesta_view.php");
+    require_once("../views/alquilar_view.php");
 ?>
