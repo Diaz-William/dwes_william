084c762423822d310f8d331e96ee0826e0b9b5d7
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-15 10:02:42 +0100

    .

--- MVC/controllers/alquilar_controller.php
+++ MVC/controllers/alquilar_controller.php
@@ -7,12 +7,14 @@
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (isset($_POST["agregar"]) && !empty($_POST["vehiculos"])) {
-            $mensaje = cesta($_POST["vehiculos"]);
-        } else if (isset($_POST["alquilar"])) {
-            //realizarAlquiler();
+            $auxAgregar = cesta($_POST["vehiculos"]);
+        } else if (isset($_POST["alquilar"]) && isset($_COOKIE["cesta"])) {
+            $alquilados = comprobarAlquilados();
             $cesta = unserialize($_COOKIE["cesta"]);
-            foreach ($cesta as $matricula => $datos) {
-                echo $matricula;
+            if ((count($cesta) + $alquilados) >= 3) {
+                $auxAlquilar = false;
+            } else {
+                $auxAlquilar = realizarAlquiler($cesta);
             }
         } else if (isset($_POST["vaciar"])) {
             vaciarCesta();
@@ -27,8 +29,16 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (isset($_POST["agregar"]) && empty($_POST["vehiculos"])) {
             echo "Debe seleccionar un vehículo";
-        } else if (isset($mensaje) && !$mensaje) {
+        } else if (isset($auxAgregar) && !$auxAgregar) {
             echo "No puede seleccionar más de 3 vehículos";
+        } else if (isset($_POST["alquilar"]) && !isset($_COOKIE["cesta"])) {
+            echo "Debe añadir vehículos a la cesta";
+        } else if (isset($auxAlquilar) && $auxAlquilar) {
+            echo "El alquiler se ha realizado correctamente";
+        } else if (isset($auxAlquilar) && !$auxAlquilar && ($alquilados - count($cesta)) == 0) {
+            echo "Ya tiene 3 vehículos alquilados";
+        } else if (isset($auxAlquilar) && !$auxAlquilar) {
+            echo "Tiene $alquilados vehículos alquilados, solo puede alquilar ($alquilados - count($cesta)) vehículos";
         }
     }
 ?>
--- MVC/models/realizarAlquiler_model.php
+++ MVC/models/realizarAlquiler_model.php
@@ -1,26 +1,28 @@
 <?php
-    function realizarAlquiler() {
+    function realizarAlquiler($cesta) {
         try {
-            $cesta = unserialize($_COOKIE["cesta"]);
-            foreach ($cesta as $matricula) {
-                echo $matricula;
-            }
             $conn = conectar();
             $conn->beginTransaction();
-            $stmt = $conn->prepare("UPDATE RVEHICULOS SET DISPONIBLE = 'N' WHERE MATRICULA = :MATRICULA");
-            $stmt->bindParam("MATRICULA", $matricula);
-            $stmt->execute();
-            $stmt = $conn->prepare("INSERT INTO RALQUILERES (IDCLIENTE, MATRICULA, FECHA_ALQUILER, FECHA_DEVOLUCION, PRECIOTOTAL, FECHAHORAPAGO) VALUES (:IDCLIENTE, :MATRICULA, :FECHA_ALQUILER, null, null, null)");
             $id = explode("#", $_COOKIE["datos"]);
             $id = $id[1];
-            $stmt->bindParam(':IDCLIENTE', $id);
-            $stmt->bindParam(':MATRICULA', $matricula);
-            $fecha_alquiler = date("Y-m-d H:i:s");
-            $stmt->bindParam(':FECHA_ALQUILER', $fecha_alquiler);
-            $stmt->execute();
+            foreach ($cesta as $matricula => $datos) {
+                $stmt = $conn->prepare("UPDATE RVEHICULOS SET DISPONIBLE = 'N' WHERE MATRICULA = :MATRICULA");
+                $stmt->bindParam(":MATRICULA", $matricula);
+                $stmt->execute();
+                $stmt = $conn->prepare("INSERT INTO RALQUILERES (IDCLIENTE, MATRICULA, FECHA_ALQUILER, FECHA_DEVOLUCION, PRECIOTOTAL, FECHAHORAPAGO) VALUES (:IDCLIENTE, :MATRICULA, :FECHA_ALQUILER, null, null, null)");
+                $stmt->bindParam(':IDCLIENTE', $id);
+                $stmt->bindParam(':MATRICULA', $matricula);
+                $fecha_alquiler = date("Y-m-d H:i:s");
+                $stmt->bindParam(':FECHA_ALQUILER', $fecha_alquiler);
+                $stmt->execute();
+            }
             $conn->commit();
+            $conn = null;
+            return true;
         } catch (PDOException $e) {
-            $conn->rollBack();
+            if ($conn) {
+                $conn->rollBack();
+            }
             $conn = null;
             error_function($e->getCode(), $e->getMessage(), $e->getFile(), $e->getLine());
             return null;
