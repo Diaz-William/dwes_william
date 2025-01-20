7e4bf1f7e54c8912c4b19101a8ae1a2d6504df87
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-20 09:24:51 +0100

    .

--- MVC/controllers/devolver_controller.php
+++ MVC/controllers/devolver_controller.php
@@ -4,7 +4,13 @@
     set_error_handler("error_function");
     require_once("../models/devolver_model.php");
 
+    date_default_timezone_set('Europe/Madrid');
     $alquilados = obtenerVehiculosAlquilados();
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $fecha_devolver = date("Y-m-d H:i:s");
+        var_dump($fecha_devolver);
+    }
 
     require_once("../views/devolver_view.php");
 ?>
