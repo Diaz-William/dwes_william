cc737ab9523ba216e954ac67d3d8620518790df2
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-20 12:03:56 +0100

    .

--- MVC/models/obtenerPrecio_model.php
+++ MVC/models/obtenerPrecio_model.php
@@ -4,12 +4,18 @@
             $id = explode("#", $_COOKIE["datos"]);
             $id = $id[1];
             $conn = conectar();
-            $stmt = $conn->prepare("SELECT ROUND((TIMESTAMPDIFF(MINUTE, RA.FECHA_ALQUILER, :FECHA_DEVOLVER) * RV.PRECIOBASE)) AS TOTAL FROM RVEHICULOS RV, RALQUILERES RA WHERE RV.MATRICULA = RA.MATRICULA AND RV.MATRICULA = :MATRICULA AND RA.FECHA_DEVOLUCION IS NULL AND RA.PRECIOTOTAL IS NULL AND RA.FECHAHORAPAGO IS NULL AND RA.IDCLIENTE = :IDCLIENTE");
+            $stmt = $conn->prepare("SELECT ROUND((TIMESTAMPDIFF(MINUTE, RA.FECHA_ALQUILER, :FECHA_DEVOLVER) * RV.PRECIOBASE), 2) AS TOTAL FROM RVEHICULOS RV, RALQUILERES RA WHERE RV.MATRICULA = RA.MATRICULA AND RV.MATRICULA = :MATRICULA AND RA.FECHA_DEVOLUCION IS NULL AND RA.PRECIOTOTAL IS NULL AND RA.FECHAHORAPAGO IS NULL AND RA.IDCLIENTE = :IDCLIENTE");
             $stmt->bindParam(":IDCLIENTE", $id);
             $stmt->bindParam(":FECHA_DEVOLVER", $fecha_devolver);
             $stmt->bindParam(":MATRICULA", $matricula);
             $stmt->execute();
             $result = $stmt->fetchColumn();
+
+            var_dump($matricula);
+            var_dump($fecha_devolver);
+            var_dump($id);
+            var_dump($result);
+
             $conn = null;
             return $result;
         } catch (PDOException $e) {
