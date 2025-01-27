bd1f77720a8caef83516e9495feb727a3a8b3e5d
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-27 10:46:50 +0100

    .

--- employees/controllers/altaEmple_controller.php
+++ employees/controllers/altaEmple_controller.php
@@ -1,11 +1,11 @@
 <?php
-    require_once("helpers/error_helper.php");
+    require_once("../helpers/error_helper.php");
     set_error_handler("error_function");
-    require_once("db/db.php");
-    require_once("helpers/data_helper.php");
-    require_once("views/altaEmple_view.php");
-    require_once("models/altaEmple.php");
-    require_once("helpers/cookie_helper.php");
+    require_once("../db/db.php");
+    require_once("../helpers/data_helper.php");
+    require_once("../views/altaEmple_view.php");
+    require_once("../models/altaEmple.php");
+    require_once("../helpers/cookie_helper.php");
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $name = test_input($_POST["name"]);
