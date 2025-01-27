e7c42af75b3dde2ba050909eb9d8593ec2e9e92d
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-27 10:12:23 +0100

    .

--- employees/controllers/welcomeRRHH_controller.php
+++ employees/controllers/welcomeRRHH_controller.php
@@ -1,6 +1,6 @@
 <?php
     require_once("../helpers/error_helper.php");
     set_error_handler("error_function");
-    require_once("../views/welcomeRRHH_view.phpS");
+    require_once("../views/welcomeRRHH_view.php");
     require_once("../helpers/cookie_helper.php");
 ?>
