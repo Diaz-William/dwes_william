26169a652b7aa667bbca97739d368303183cd2e6
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-28 10:42:38 +0100

    .

--- employees/views/welcomeRRHH_view.php
+++ employees/views/welcomeRRHH_view.php
@@ -27,7 +27,7 @@
 	
 		<input type="button" value="Alta Empleado" onclick="window.location.href='altaEmple_controller.php'" class="btn btn-warning disabled">
 		<input type="button" value="Alta Masiva Empleados" onclick="window.location.href='altaMasiva_controller.php'" class="btn btn-warning disabled">
-		<input type="button" value="Modificar Salario" onclick="window.location.href='devolver_controller.php'" class="btn btn-warning disabled">
+		<input type="button" value="Modificar Salario" onclick="window.location.href='cambiarSalario_controller.php'" class="btn btn-warning disabled">
 		<input type="button" value="Vida Laboral" onclick="window.location.href='alquilar_controller.php'" class="btn btn-warning disabled">
 		<input type="button" value="Info Departamentos" onclick="window.location.href='consultar_controller.php'" class="btn btn-warning disabled">
 		<input type="button" value="Cambio Departamento" onclick="window.location.href='devolver_controller.php'" class="btn btn-warning disabled">
