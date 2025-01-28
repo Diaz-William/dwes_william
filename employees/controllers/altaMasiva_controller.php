ac62c99ce1db30012b846c10dfd2b7758ad3c3e3
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-28 09:45:14 +0100

    .

--- employees/controllers/altaMasiva_controller.php
+++ employees/controllers/altaMasiva_controller.php
@@ -22,11 +22,22 @@
         } else if (isset($_POST["hire"]) && isset($_COOKIE["basketEmp"])) {
             require_once("../models/altaEmple_model.php");
             $basketEmp = unserialize($_COOKIE["basketEmp"]);
+            $errorEmp = array();
+            $indexError = 0;
             foreach ($basketEmp as $index => $dataEmp) {
                 list($birthdate, $firstname, $lastname, $gender, $deptno, $salary, $title) = explode("#", $dataEmp);
                 $alta = altaEmple($birthdate, $firstname, $lastname, $gender, $deptno, $salary, $title);
                 if ($alta === null) {
+                    $errorEmp[$indexError] = $dataEmp;
+                    $indexError += 1;
                     echo "El nuevo empleado con la posición " . $index + 1 . " en la cesta no se ha contratado";
+                }
+            }
+            vaciarbasketEmp();
+            if (count($errorEmp) !== 0) {
+                foreach ($errorEmp as $index => $dataEmp) {
+                    list($birthdate, $firstname, $lastname, $gender, $deptno, $salary, $title) = explode("#", $dataEmp);
+                    basketEmp($birthdate, $firstname, $lastname, $gender, $deptno, $salary, $title);
                 }
             }
         } else {
