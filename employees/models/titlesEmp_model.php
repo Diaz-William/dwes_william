9acebdd9978de4127a51847846cf8d432c0eab50
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-28 13:30:24 +0100

    .

--- employees/models/titlesEmp_model.php
+++ employees/models/titlesEmp_model.php
@@ -2,7 +2,7 @@
     function getTitEmp($empno) {
         try {
             $conn = conectar();
-            $stmt = $conn->prepare("SELECT CONCAT('Cargo de ', TITLE, ' desde ', FROM_DATE, ' hasta ', COALESCE(TO_DATE,'la actualidad'))) AS INFO FROM TITLES WHERE EMP_NO = :EMP_NO");
+            $stmt = $conn->prepare("SELECT CONCAT('Cargo de ', TITLE, ' desde ', FROM_DATE, ' hasta ', COALESCE(TO_DATE,'la actualidad')) AS INFO FROM TITLES WHERE EMP_NO = :EMP_NO");
             $stmt->bindParam(":EMP_NO", $empno);
             $stmt->execute();
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
