01200a336f797379a91201c2c4ce1808b0ff4958
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-29 16:21:15 +0100

    .

--- employees/models/getDeptData_model.php
+++ employees/models/getDeptData_model.php
@@ -10,7 +10,7 @@
             $result = $stmt->fetchAll();
             $stmt = $conn->prepare("SELECT E.EMP_NO, CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS FULLNAME FROM EMPLOYEES E, DEPT_EMP DE WHERE E.EMP_NO = DE.EMP_NO AND DE.DEPT_NO = :DEPT_NO AND E.EMP_NO != :MANAGER_EMP_NO");
             $stmt->bindParam(":DEPT_NO", $deptno);
-            $stmt->bindParam(":MANAGER_EMP_NO", $manager['MANAGER']);
+            $stmt->bindParam(":MANAGER_EMP_NO", $result['MANAGER']);
             $stmt->execute();
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
             $result = array_merge($result, $stmt->fetchAll());
