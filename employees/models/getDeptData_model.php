c0c6dbf4958caadcdf5af4546c887246e6fa3b10
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-29 16:08:30 +0100

    .

--- employees/models/getDeptData_model.php
+++ employees/models/getDeptData_model.php
@@ -2,16 +2,18 @@
     function getDeptData($deptno) {
         try {
             $conn = conectar();
+            $result = array();
             $stmt = $conn->prepare("SELECT E.EMP_NO, E.FIRST_NAME, E.LAST_NAME FROM EMPLOYEES E, DEPT_MANAGER DM WHERE E.EMP_NO = DM.EMP_NO AND DM.DEPT_NO = :DEPT_NO");
-            $stmt->bindParam("DEPT_NO", $deptno);
+            $stmt->bindParam(":DEPT_NO", $deptno);
             $stmt->execute();
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
             $result = $stmt->fetchAll();
             $stmt = $conn->prepare("SELECT E.EMP_NO, E.FIRST_NAME, E.LAST_NAME FROM EMPLOYEES E, DEPT_EMP DE WHERE E.EMP_NO = DE.EMP_NO AND DE.DEPT_NO = :DEPT_NO");
-            $stmt->bindParam("DEPT_NO", $deptno);
+            $stmt->bindParam(":DEPT_NO", $deptno);
             $stmt->execute();
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
-            $result += $stmt->fetchAll();
+            //$result += $stmt->fetchAll();
+            $result = array_merge($result, $stmt->fetchAll());
             $conn = null;
             return $result;
         } catch (PDOException $e) {
