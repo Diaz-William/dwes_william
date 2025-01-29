974f15afa7972d7bf929abed35887f4b2d13e09a
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-29 16:16:14 +0100

    .

--- employees/models/getDeptData_model.php
+++ employees/models/getDeptData_model.php
@@ -3,12 +3,12 @@
         try {
             $conn = conectar();
             $result = array();
-            $stmt = $conn->prepare("SELECT E.EMP_NO AS MANAGER, E.FIRST_NAME, E.LAST_NAME FROM EMPLOYEES E, DEPT_MANAGER DM WHERE E.EMP_NO = DM.EMP_NO AND DM.DEPT_NO = :DEPT_NO");
+            $stmt = $conn->prepare("SELECT E.EMP_NO AS MANAGER, CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS FULLNAME FROM EMPLOYEES E, DEPT_MANAGER DM WHERE E.EMP_NO = DM.EMP_NO AND DM.DEPT_NO = :DEPT_NO");
             $stmt->bindParam(":DEPT_NO", $deptno);
             $stmt->execute();
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
             $result = $stmt->fetchAll();
-            $stmt = $conn->prepare("SELECT E.EMP_NO, E.FIRST_NAME, E.LAST_NAME FROM EMPLOYEES E, DEPT_EMP DE WHERE E.EMP_NO = DE.EMP_NO AND DE.DEPT_NO = :DEPT_NO");
+            $stmt = $conn->prepare("SELECT E.EMP_NO, CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS FULLNAME FROM EMPLOYEES E, DEPT_EMP DE WHERE E.EMP_NO = DE.EMP_NO AND DE.DEPT_NO = :DEPT_NO");
             $stmt->bindParam(":DEPT_NO", $deptno);
             $stmt->execute();
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
