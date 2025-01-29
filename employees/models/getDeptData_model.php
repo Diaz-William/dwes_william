86039af5ab2dbe9bb50f11348055f37866f13d62
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-29 16:09:35 +0100

    .

--- employees/models/getDeptData_model.php
+++ employees/models/getDeptData_model.php
@@ -3,7 +3,7 @@
         try {
             $conn = conectar();
             $result = array();
-            $stmt = $conn->prepare("SELECT E.EMP_NO, E.FIRST_NAME, E.LAST_NAME FROM EMPLOYEES E, DEPT_MANAGER DM WHERE E.EMP_NO = DM.EMP_NO AND DM.DEPT_NO = :DEPT_NO");
+            $stmt = $conn->prepare("SELECT E.EMP_NO AS MANAGER, E.FIRST_NAME, E.LAST_NAME FROM EMPLOYEES E, DEPT_MANAGER DM WHERE E.EMP_NO = DM.EMP_NO AND DM.DEPT_NO = :DEPT_NO");
             $stmt->bindParam(":DEPT_NO", $deptno);
             $stmt->execute();
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
