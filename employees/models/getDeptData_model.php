b7dd30c2941fdba0119dbf69b37d2cd6d36f3aaa
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-29 15:46:43 +0100

    .

--- employees/models/getDeptData_model.php
+++ employees/models/getDeptData_model.php
@@ -2,7 +2,7 @@
     function getDeptData($deptno) {
         try {
             $conn = conectar();
-            $stmt = $conn->prepare("SELECT D.DEPT_NO AS 'Número de Departamento', D.DEPT_NAME AS 'Nombre del Departamento', DM.EMP_NO AS 'Número de Manager', DE.EMP_NO AS 'Número de Empleado' FROM DEPARTMENTS D, DEPT_MANAGER DM, DEPT_EMP DE WHERE D.DEPT_NO = DM.DEPT_NO AND D.DEPT_NO = DE.DEPT_NO AND D.DEPT_NO = :DEPT_NO ORDER BY D.DEPT_NO, DM.EMP_NO, DE.EMP_NO;");
+            $stmt = $conn->prepare("SELECT CONCAT('Departamento: ', D.DEPT_NAME, ' (', D.DEPT_NO, '), ', 'Manager: ', COALESCE(DM.EMP_NO, 'Sin manager'), ', ', 'Empleado: ', DE.EMP_NO) AS INFO FROM DEPARTMENTS D, DEPT_MANAGER DM, DEPT_EMP DE WHERE D.DEPT_NO = DM.DEPT_NO AND D.DEPT_NO = DE.DEPT_NO AND D.DEPT_NO = :DEPT_NO OR DM.DEPT_NO IS NULL OR DE.DEPT_NO IS NULL ORDER BY D.DEPT_NO, DM.EMP_NO, DE.EMP_NO");
             $stmt->bindParam("DEPT_NO", $deptno);
             $stmt->execute();
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
