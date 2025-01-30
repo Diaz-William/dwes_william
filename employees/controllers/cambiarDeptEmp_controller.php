398d36015fec6c68d06c0b20dd0c206c45ae0ce9
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-30 09:34:22 +0100

    .

--- employees/controllers/cambiarDeptEmp_controller.php
+++ employees/controllers/cambiarDeptEmp_controller.php
@@ -9,6 +9,9 @@
     $empleados = obtenerEmpleados();
     $depts = obtenerDept();
 
+    var_dump($empleados);
+    var_dump($depts);
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (isset($_POST["change"]) && !empty($_POST["empdata"]) && !empty($_POST["deptdata"])) {
             list($empno, $empname) = explode("#", test_input($_POST["empdata"]));
