13bc14c1f242676f5b568c80246e501e2d0199ae
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-30 12:47:07 +0100

    .

--- employees/models/getEmpData_model.php
+++ employees/models/getEmpData_model.php
@@ -5,7 +5,7 @@
             $stmt = $conn->prepare("SELECT BIRTH_DATE, FIRST_NAME, LAST_NAME, IF(GENDER = 'M', 'Hombre', 'Mujer') AS GENDER, HIRE_DATE FROM EMPLOYEES WHERE EMP_NO = :EMP_NO");
             $stmt->bindParam(":EMP_NO", $empno);
             $stmt->execute();
-            //$stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
             $result = $stmt->fetchAll();
             return $result;
         } catch (PDOException $e) {
