269d4e4da5ec07fbc53fad4f7d7d36b07b98f7dd
William Diaz <williameliasdiazsantana@gmail.com>
2025-01-30 13:22:48 +0100

    .

--- employees/models/comprobarEngineer_model.php
+++ employees/models/comprobarEngineer_model.php
@@ -2,7 +2,7 @@
     function comprobarEngineer($empno) {
         try {
             $conn = conectar();
-            $stmt = $conn->prepare("SELECT 1 FROM TITLES WHERE EMP_NO = :EMP_NO AND UPPER(TITLE) LIKE '%ENGINEER%'");
+            $stmt = $conn->prepare("SELECT 1 FROM TITLES WHERE EMP_NO = :EMP_NO AND UPPER(TITLE) LIKE '%ENGINEER%' AND TO_DATE IS NULL");
             $stmt->bindParam(":EMP_NO", $empno);
             $stmt->execute();
             $result = $stmt->fetchColumn();
