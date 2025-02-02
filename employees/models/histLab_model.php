40bd133e34807a7fd3f1ecc25c633e8fd2cf1d3f
William Diaz <williameliasdiazsantana@gmail.com>
2025-02-02 10:54:23 +0100

    .

--- employees/models/histLab_model.php
+++ employees/models/histLab_model.php
@@ -2,7 +2,7 @@
     function getEmpHistory($empno) {
         try {
             $conn = conectar();
-            $stmt = $conn->prepare("SELECT D.DEPT_NAME, DE.FROM_DATE AS DEPT_FROM, DE.TO_DATE AS DEPT_TO, S.SALARY, S.FROM_DATE AS SAL_FROM, S.TO_DATE AS SAL_TO FROM DEPT_EMP DE, DEPARTMENTS D, SALARIES S WHERE DE.DEPT_NO = D.DEPT_NO AND DE.EMP_NO = S.EMP_NO AND DE.EMP_NO = :EMP_NO ORDER BY DE.FROM_DATE DESC, S.FROM_DATE DESC");
+            $stmt = $conn->prepare("SELECT CONCAT(D.DEPT_NAME, ', desde ', DE.FROM_DATE, ', hasta ', COALESCE(DE.TO_DATE, 'la actualidad'), ', salario: ', S.SALARY, ' desde ', S.FROM_DATE, ' hasta ', COALESCE(S.TO_DATE, 'la actualidad')) AS INFO FROM DEPT_EMP DE, DEPARTMENTS D, SALARIES S WHERE DE.DEPT_NO = D.DEPT_NO AND DE.EMP_NO = S.EMP_NO AND DE.EMP_NO = :EMP_NO ORDER BY DE.FROM_DATE DESC, S.FROM_DATE DESC");
             $stmt->bindParam(":EMP_NO", $empno);
             $stmt->execute();
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -13,7 +13,7 @@
                 $conn = null;
             }
             error_function($e->getCode(), $e->getMessage(), $e->getFile(), $e->getLine());
-            return null;
+            return [];
         }
     }
 ?>
