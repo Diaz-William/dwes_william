ca7904e73499ce61fc00649f4b22c366601b8e02
William Diaz <williameliasdiazsantana@gmail.com>
2025-02-13 22:04:44 +0100

    .

--- musica/controllers/purchases_controller.php
+++ musica/controllers/purchases_controller.php
@@ -2,7 +2,7 @@
     require_once("../helpers/error_helper.php");
     set_error_handler("error_function");
     require_once("../helpers/data_helper.php");
-    require_once("../models/purchases_model.php");
+    //require_once("../models/purchases_model.php");
     require_once("../helpers/cookie_helper.php");
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
