ffbac21b9472c2482713fff133bcc50126bd191c
William Diaz <williameliasdiazsantana@gmail.com>
2025-02-14 10:15:46 +0100

    .

--- musica/controllers/histInvoice_controller.php
+++ musica/controllers/histInvoice_controller.php
@@ -2,12 +2,8 @@
     require_once("../helpers/error_helper.php");
     set_error_handler("error_function");
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_POST["add"]) && !empty($_POST["trackinfo"])) {
-            require_once("../models/getInvoice_model.php");
-            $invoices = getInvoice();
-        }
-    }
+    require_once("../models/getInvoice_model.php");
+    $invoices = getInvoice();
 
     require_once("../views/histInvoice_view.php");
 ?>
