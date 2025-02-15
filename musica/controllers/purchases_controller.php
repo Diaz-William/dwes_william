3d64b314d1160629840dd00672f310503deb7eb2
William Diaz <williameliasdiazsantana@gmail.com>
2025-02-15 12:22:36 +0100

    .

--- musica/controllers/purchases_controller.php
+++ musica/controllers/purchases_controller.php
@@ -3,12 +3,9 @@
     set_error_handler("error_function");
     require_once("../helpers/data_helper.php");
     //require_once("../models/purchases_model.php");
-    require_once("../helpers/cookie_helper.php");
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_POST["add"]) && !empty($_POST["trackinfo"])) {
-            $added = basketTracks($trackinfo);
-        }
+        //code...
     }
 
     require_once("../views/purchases_view.php");
