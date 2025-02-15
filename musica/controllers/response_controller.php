963244d8a5bb4fc643e561d62195a9d463c360cb
William Diaz <williameliasdiazsantana@gmail.com>
2025-02-15 17:29:45 +0100

    .

--- musica/controllers/response_controller.php
+++ musica/controllers/response_controller.php
@@ -9,7 +9,7 @@
     $params = $_REQUEST["Ds_MerchantParameters"]; 
     $signatureRecibida = $_REQUEST["Ds_Signature"];
 
-    $decodec = $miObj->decodeMerchantParameters($params);
+    $decodec = json_decode(urldecode($params), true);
     var_dump($decodec);
 
     $codigoRespuesta = $miObj->getParameter("Ds_Response");
@@ -17,12 +17,15 @@
     $claveModuloAdmin = 'sq7HjrUOBfKmC576ILgskD5srU870gJ7'; 
     $signatureCalculada = $miObj->createMerchantSignatureNotif($claveModuloAdmin, $params);
 
-    $invoiceid = $miObj->getOrder();
-    $invoicedate = date("Y-m-d", strtotime($miObj->getParameter("DS_DATE")));
-    $amount = $miObj->getParameter("DS_AMOUNT");
-    $cardnumber = $miObj->getParameter("DS_CARD_NUMBER");
-    $country = $miObj->getParameter("DS_CARD_COUNTRY");
-    var_dump($country);
+    // Verifica si los valores realmente están definidos
+    $invoiceid = isset($decodec["Ds_Order"]) ? $decodec["Ds_Order"] : null;
+    $invoicedate = isset($decodec["Ds_Date"]) ? date("Y-m-d", strtotime(str_replace("/", "-", $decodec["Ds_Date"]))) : null;
+    $amount = isset($decodec["Ds_Amount"]) ? $decodec["Ds_Amount"] : null;
+    $cardnumber = isset($decodec["Ds_Card_Number"]) ? $decodec["Ds_Card_Number"] : null;
+    $country = isset($decodec["Ds_Card_Country"]) ? $decodec["Ds_Card_Country"] : null;
+
+    // Imprimir los valores para asegurarnos de que no son NULL
+    var_dump($invoiceid, $invoicedate, $amount, $cardnumber, $country);
 
     require_once("../db/db.php");
     require_once("../models/payment_model.php");
