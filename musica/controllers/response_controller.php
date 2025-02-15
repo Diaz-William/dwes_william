e55f0765743ab1951d2c00e630302cfdd7ca8186
William Diaz <williameliasdiazsantana@gmail.com>
2025-02-15 18:04:04 +0100

    .

--- musica/controllers/response_controller.php
+++ musica/controllers/response_controller.php
@@ -1,5 +1,5 @@
 <?php
-    /*require_once("../helpers/error_helper.php");
+    require_once("../helpers/error_helper.php");
     set_error_handler("error_function");
 
     require_once("./apiRedsys.php");
@@ -23,7 +23,7 @@
     $invoiceid = isset($decodec["Ds_Order"]) ? $decodec["Ds_Order"] : null;
     $invoicedate = isset($decodec["Ds_Date"]) ? date("Y-m-d", strtotime(str_replace("/", "-", urldecode($decodec["Ds_Date"])))) : null;
     $amount = isset($decodec["Ds_Amount"]) ? $decodec["Ds_Amount"] : null;
-    $cardnumber = isset($decodec["Ds_Card_Number"]) ? $decodec["Ds_Card_Number"] : null;
+    $cardnumber = isset($decodec["Ds_CardNumber"]) ? $decodec["Ds_CardNumber"] : null;
     $country = isset($decodec["Ds_Card_Country"]) ? $decodec["Ds_Card_Country"] : null;
 
     // Imprimir los valores para asegurarnos de que no son NULL
@@ -35,41 +35,6 @@
         //pay($invoiceid, $invoicedate, $amount, $cardnumber, 0);
     } else {
         //pay($invoiceid, $invoicedate, $amount, $cardnumber, 1);
-    }
-
-    require_once("../views/response_view.php");*/
-?>
-
-<?php
-    require_once("./apiRedsys.php");
-    $miObj = new RedsysAPI;
-
-    $version = $_REQUEST["Ds_SignatureVersion"];
-    $params = $_REQUEST["Ds_MerchantParameters"];
-    $signatureRecibida = $_REQUEST["Ds_Signature"];
-
-    $decodec = $miObj->decodeMerchantParameters($params);
-    var_dump($decodec);
-
-    $codigoRespuesta = $miObj->getParameter("Ds_Response");
-    $cardnumber = $miObj->getParameter("Ds_CardNumber"); // Obtiene los últimos cuatro dígitos de la tarjeta
-
-    $claveModuloAdmin = 'sq7HjrUOBfKmC576ILgskD5srU870gJ7';
-    $signatureCalculada = $miObj->createMerchantSignatureNotif($claveModuloAdmin, $params);
-
-    require_once("../db/db.php");
-    require_once("../models/payment_model.php");
-    $invoiceid = $miObj->getOrder();
-    $invoicedate = date("Y-m-d", strtotime($miObj->getParameter("Ds_Date")));
-    $amount = $miObj->getParameter("Ds_Amount");
-    $country = $miObj->getParameter("Ds_Card_Country");
-
-    var_dump($invoiceid, $invoicedate, $amount, $cardnumber, $country);
-
-    if ($signatureCalculada === $signatureRecibida && $codigoRespuesta >= 0 && $codigoRespuesta < 100) {
-        //pay($invoiceid, $invoicedate, $amount, $cardnumber, 0); // Pasa los últimos cuatro dígitos a la función pay
-    } else {
-        //pay($invoiceid, $invoicedate, $amount, $cardnumber, 1); // Pasa los últimos cuatro dígitos a la función pay
     }
 
     require_once("../views/response_view.php");
